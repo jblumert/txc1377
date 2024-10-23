87c148c36ed0fc9f7d906360c332901f9464aa08
jblumert <66739278+jblumert@users.noreply.github.com>
2024-10-23 07:04:05 -0700

    add patch

--- gpfspatch.sh
+++ gpfspatch.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+echo "unmounting fs1"
 mmumount fs1
+echo "deleting fs1"
 mmdelfs1
+echo "creating fs1"
 mmcrfs fs1 -F nsd -A yes -D nfs4 –version “5.2.0.0” -j cluster -Q yes -S relatime -T /scale/fs1
+echo "mounting fs1"
 mmmount fs1
+echo "checking fs1"
+df -h
+mmlsfs fs1
+echo setting quota for fs1
 mmquotaon fs1
 mmdefquotaon fs1
 mmquotaon -a
@@ -11,5 +19,6 @@ mmchconfig enforceFilesetQuoteOnRoot=yes -i
 mmchconfig controlSetxattrImmutableSELinux=yes -i
 mmchfs fs1 --filesetdf
 mmchfs fs1 ---auto-inode-limit
+echo "disabling firewall"
 systemctl disable firewalld
 systemctl stop firewalld
