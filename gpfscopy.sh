c88f7ed9a05852980751b04ef00444f0699a4ae9
jblumert <66739278+jblumert@users.noreply.github.com>
2024-10-23 06:54:27 -0700

    add patch

--- gpfscopy.sh
+++ gpfscopy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sshpass -p 'Passw0rd!' scp -o StrictHostKeyChecking=no -o StrictHostKeyChecking=no root@10.100.1.5:
-sshpass -p 'Passw0rd!' ssh -o StrictHostKeyChecking=no root@10.100.1.5 gpfspatch.sh
+sshpass -p 'Passw0rd' scp -o StrictHostKeyChecking=no gpfspatch.sh root@10.100.1.5:
+sshpass -p 'Passw0rd' ssh -o StrictHostKeyChecking=no root@10.100.1.5 gpfspatch.sh
